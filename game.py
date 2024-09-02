18f9fcaf31d59e507aa0a5162856d4a63e30ba11
AryanMahajan <mahajanaryan0409@gmail.com>
2024-09-02 06:28:39 +0530

    NEAT

--- game.py
+++ game.py
@@ -326,9 +326,6 @@ def run(config_file):
 
 
 if __name__ == '__main__':
-    # Determine path to configuration file. This path manipulation is
-    # here so that the script will run successfully regardless of the
-    # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     run(config_path)
